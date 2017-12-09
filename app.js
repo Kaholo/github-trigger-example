5b1a4b6cf9bc941394c8db81b3c9ab840a8ba984
talyo10 <32864530+talyo10@users.noreply.github.com>
2017-12-09 23:39:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello   W    orl  d!'));
+app.get('/', (req, res) => res.send('Hello       W    orl  d!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
